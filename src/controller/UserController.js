856e8b18951b250c83c4bad73f4bc8cacd864275
Caioscg <cacazaok5@gmail.com>
2023-03-11 18:22:43 -0300

    delete users

--- src/controller/UserController.js
+++ src/controller/UserController.js
@@ -76,6 +76,13 @@ class UsersControllers {
 
     }
 
+    async delete(req, res) {
+        const { id } = req.params
+        await knex("users").where({ id }).delete()
+
+        return res.json()
+    }
+
 }
 
 module.exports = UsersControllers
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -8,5 +8,6 @@ const usersRoutes = Router()
 
 usersRoutes.post("/", UserController.create)
 usersRoutes.put("/:id", UserController.update)
+usersRoutes.delete("/:id", UserController.delete)
 
 module.exports = usersRoutes
